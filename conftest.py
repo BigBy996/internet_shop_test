b489d1541ff1ebb0b77773082c2fc3056b5d1ad4
Aleksandr Khorev <aleksandr.khorev@valantmed.com>
2019-06-20 11:05:34 +0300

    add conftest.py

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='ru',
+                     help='Choose language')
+
+
+@pytest.fixture()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        print("Browser {} still is not implemented".format(browser_name))
+        browser = None
+    yield browser
+    print("\nquit browser...")
+    browser.quit()
