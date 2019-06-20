0af0b8f0379e2e7a09b5bb1c213e88254c2c4b98
BigBy996 <aleksandr.khorev@valantmed.com>
2019-06-20 11:46:41 +0300

    use browser configuration for language instead going to proper link

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
@@ -12,10 +13,15 @@ def pytest_addoption(parser):
 @pytest.fixture()
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
     if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language })
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
+        firefox_profile = webdriver.FirefoxProfile()
+        firefox_profile.set_preference("intl.accept_languages", user_language)
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox()
     else:
--- test_items.py
+++ test_items.py
@@ -1,7 +1,4 @@
-link_template = "http://selenium1py.pythonanywhere.com/{}/catalogue/coders-at-work_207/"
-
-
-def test_contains_add_to_basket_button(browser, request):
-    language = request.config.getoption("language")
-    browser.get(link_template.format(language))
+def test_contains_add_to_basket_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
     browser.find_element_by_class_name("btn-add-to-basket")
